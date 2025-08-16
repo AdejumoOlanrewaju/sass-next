3a419136bcbc9fb035d337b2eca876db28dd4f5e
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2025-08-16 10:15:36 +0100

    upload to github

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,20 +9,22 @@ const Navbar = async () => {
         <header className='sticky top-0 h-15'>
             <nav className='flex justify-between h-full bg-gray-100 items-center px-4'>
                 <Link href={'/'}>
-                   <span className='font-medium italic text-xl tracking-[1px]'>SaasNext</span>
+                    <span className='font-medium italic text-xl tracking-[1px]'>SaasNext</span>
                 </Link>
 
-                <ul className='list-none pl-0'>
+                <ul className='list-none pl-0 flex gap-3 '>
                     {
                         session && session?.user ? (
-                            <div>
-                                <Link href = '/startup/create' className=''>Create</Link>
-                                <Link href = {`user/${session?.user.id}`}></Link>
+                            <div className='flex gap-3 items-center'>
+                                <Link href='/startup/create' className='px-3 py-2 bg-black/90 text-white rounded-md'>Create</Link>
+                                <span className='w-8 h-8 rounded-full bg-green-800 flex items-center justify-center'>
+                                    <Link className='text-white' href={`user/${session.user.id}`}>{session.user.name?.slice(0, 1)}</Link>
+                                </span>
                                 <form action={async () => {
                                     "use server"
                                     await signOut()
                                 }}>
-                                    <button type = "submit">Logout</button>
+                                    <button type="submit">Logout</button>
                                 </form>
                             </div>
                         ) : (
